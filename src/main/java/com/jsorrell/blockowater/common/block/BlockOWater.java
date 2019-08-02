290f9dd8e8ee76a561770bcafc679e7fbcb341c7
Jack Sorrell <jack@jacksorrell.com>
2019-08-02 14:25:02 -0400

    Allow water to be removed with bucket/tank

--- src/main/java/com/jsorrell/blockowater/common/block/BlockOWater.java
+++ src/main/java/com/jsorrell/blockowater/common/block/BlockOWater.java
@@ -4,8 +4,14 @@ import com.jsorrell.blockowater.common.item.ItemBlockMod;
 import com.jsorrell.blockowater.common.tileentity.TileEntityBlockOWater;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -28,5 +34,18 @@ public class BlockOWater extends BlockMod {
   @Override
   public TileEntity createTileEntity(@Nonnull World world, @Nonnull IBlockState state) {
     return new TileEntityBlockOWater();
+  }
+
+  @Override
+  public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+    boolean result = false;
+    if (player != null && hand == EnumHand.MAIN_HAND) {
+      IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(player.getHeldItem(hand));
+      if (fluidHandler != null) {
+        FluidUtil.interactWithFluidHandler(player, hand, world, pos, facing);
+        return true;
+      }
+    }
+    return super.onBlockActivated(world, pos, state, player, hand, facing, hitX, hitY, hitZ);
   }
 }
